99c7afdedb511894d7602e810d55c8ae36bf4c99
mrakotomizao <mrakotomizao@gmail.com>
2015-09-12 15:50:44 +0200

    modification bdd +  suppression de fichier inutile

--- pages/connexion.php
+++ pages/connexion.php
@@ -1,14 +1,17 @@
 <?php
-				$serveurBD = "localhost:8888";
-			    $nomUtilisateur = "root";
-			    $motDePasse = "root";
-			    $baseDeDonnees = "casino";
-			   
-			    $idConnexion = mysql_connect($serveurBD,
-			                                 $nomUtilisateur,
-			                                 $motDePasse);
-			                                 			
-			    $connexionBase = mysql_select_db($baseDeDonnees);
-				
+if(ENV=='dev'){
+    $serveurBD = "localhost:8888";
+    $nomUtilisateur = "root";
+    $motDePasse = "root";
+    $baseDeDonnees = "casino";
+}else{
+    $serveurBD = "ec2-54-163-228-0.compute-1.amazonaws.com";
+    $nomUtilisateur = "puecnxpywhyifz";
+    $motDePasse = "pONg0Qh-TgyhEJ-InlZigt9eCP";
+    $baseDeDonnees = "d1nttj2qfni6tg";
+}
+$idConnexion = mysql_connect($serveurBD,
+    $nomUtilisateur,
+    $motDePasse);
 
-?>
+$connexionBase = mysql_select_db($baseDeDonnees);
