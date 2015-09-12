d8d36fdcbf630e8d4a144f657cb685c4aeb33640
mrakotomizao <mrakotomizao@gmail.com>
2015-09-12 15:42:21 +0200

    first commit

--- facebook-connect/app/Facebook/constants.php
+++ facebook-connect/app/Facebook/constants.php
@@ -5,6 +5,11 @@
  * Date: 20/04/2015
  * Time: 23:09
  */
-define('APP_ID','1561723867399971');
-define('APP_SECRET','49fa649f1f02c4d24fb87dc2efea529d');
-define('REDIRECT_URL','http://localhost:8888/COCO/pages/page_roulette.php');
+define("ENV","prod");
+define('APP_ID','476959155844258');
+define('APP_SECRET','b3d25cbfe25ac48aca06ef4c14aaf6af');
+if(ENV=="dev"){
+    define('REDIRECT_URL','http://localhost:8888/COCO/pages/page_roulette.php');
+}else{
+    define('REDIRECT_URL','https://project-coco.herokuapp.com/');
+}
